563f2ccf23deb41a92cf3e6c1f08b31e5c0d2536
kostabuntsev <kosta.buntsev@gmail.com>
2020-12-27 14:58:46 +1100

    Added a cannon GameObject

---
+++ Assets/CannonShooting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonShooting : MonoBehaviour
+{
+    public GameObject AimingPoint;
+    public GameObject CannonBullet;
+
+    private float time = 0.0f;
+    public float period = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        time = time + Time.deltaTime;
+
+        if (time >= period)
+        {
+            time = 0.0f;
+
+            // bullet position
+            GameObject bulletObject = Instantiate(CannonBullet);
+            bulletObject.transform.position = AimingPoint.transform.position;
+
+            // initial force
+            Rigidbody rb = bulletObject.GetComponent<Rigidbody>();
+            rb.AddForce(-AimingPoint.transform.forward * 20, ForceMode.Impulse);
+            //Debug.Log(transform.forward * 100);
+
+        }
+    }
+}
